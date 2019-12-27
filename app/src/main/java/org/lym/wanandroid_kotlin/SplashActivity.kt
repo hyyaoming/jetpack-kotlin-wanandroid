4c8f19587178ababa904c2cf7126fb6c05368600
hyyaoming <m13162058226@163.com>
2019-12-28 00:18:45 +0800

    [*]create project

---
+++ app/src/main/java/org/lym/wanandroid_kotlin/SplashActivity.kt
@@ -0,0 +1,16 @@
+package org.lym.wanandroid_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * author: ym.li
+ * since: 2019/12/27
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
